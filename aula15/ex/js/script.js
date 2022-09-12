6769f5c71acd5d6ab88d4d915c1f8c66e04eec73
ogatalars <ogataogara@gmail.com>
2022-09-12 17:10:25 -0300

    exercicio com numbers e Math

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -1,13 +1,18 @@
-const numero = Number(prompt('Digite um número:'));
-const numeroTitulo = document.getElementById('numero-titulo');
-const texto = document.getElementById('texto');
+const numero = Number(prompt("Digite um número:"));
+const numeroTitulo = document.getElementById("numero-titulo");
+const texto = document.getElementById("texto");
 
 numeroTitulo.innerHTML = numero;
+let raizQuadrada = numero ** 0.5;
+let notANumber = Number.isNaN(numero);
+let inteiro = Number.isInteger(numero);
+let arrendodadoCima = Math.ceil(numero);
+let arrendondaBaixo = Math.floor(numero);
 
-texto.innerHTML = '';
-texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}.</p>`;
-texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}.</p>`;
-texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}.</p>`;
-texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}.</p>`;
-texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}.</p>`;
+texto.innerHTML = "";
+texto.innerHTML += `<p>Raiz quadrada: ${raizQuadrada}.</p>`;
+texto.innerHTML += `<p>É NaN: ${notANumber}.</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${inteiro}.</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${arrendondaBaixo}.</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${arrendodadoCima}.</p>`;
 texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}.</p>`;
